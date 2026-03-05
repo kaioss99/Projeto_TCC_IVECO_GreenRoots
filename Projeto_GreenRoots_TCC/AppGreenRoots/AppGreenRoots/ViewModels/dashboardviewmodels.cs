45d44a089a6002d3e3f70a0c856914d97a1c14c8
kaio <zaefpss@gmail.com>
2026-03-05 00:51:27 -0300

    Adicionado persistência de dados funcional e design modificado

---
+++ Projeto_GreenRoots_TCC/AppGreenRoots/AppGreenRoots/ViewModels/dashboardviewmodels.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using AppGreenRoots.Commands;
+using AppGreenRoots.Models;
+
+namespace AppGreenRoots.ViewModels;
+
+public class DashboardViewModel : INotifyPropertyChanged
+{
+    private readonly Usuario _usuario;
+    public string NomeUsuario  => _usuario.Nome;
+    public string EmailUsuario => _usuario.Email;
+
+    public ICommand LogoutCommand { get; }
+
+    public DashboardViewModel(Usuario usuario)
+    {
+        _usuario      = usuario;
+        LogoutCommand = new RelayCommand(_ => ExecutarLogout());
+    }
+
+    private void ExecutarLogout()
+    {
+        var login = new Views.LoginView();
+        login.Show();
+        foreach (Window w in Application.Current.Windows)
+            if (w is Views.DashboardView) { w.Close(); break; }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
---
+++ Projeto_GreenRoots_TCC/AppGreenRoots/AppGreenRoots/Views/DashboardView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace AppGreenRoots.Views;
+
+public partial class DashboardView : Window
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnFechar_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
